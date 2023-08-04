424dd68b795c2e1debdb0dac359f406127330907
tiger <thisisstreamliner@gmail.com>
2023-08-04 18:48:17 +0300

    Add InputHandler

--- components/PrimaryButtons.js
+++ components/PrimaryButtons.js
@@ -1,10 +1,12 @@
 import { View, Text, Pressable, StyleSheet } from "react-native"
 
-function PrimaryButton({children}) {
-    function click(){}
+function PrimaryButton({children, onPress}) {
+    function pressHandler(){
+        onPress()
+    }
 
     return <View style={styles.buttonOuterContainer}>
-        <Pressable onPress={click} android_ripple={{color: '#640233'}} style={({pressed}) => 
+        <Pressable onPress={pressHandler} android_ripple={{color: '#640233'}} style={({pressed}) => 
                 pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}>
             <Text style={styles.buttonText}>{children}</Text>
         </Pressable>
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,22 +1,35 @@
+import { useState } from 'react'
 import { TextInput, View, StyleSheet } from "react-native"
 import PrimaryButton from "../components/PrimaryButtons"
 
 
 function StartGameScreen() {
+    const [enteredNumber, setEnteredNumber] = useState('')
+
+    function numberInputHandler(inputText) {
+        setEnteredNumber(enteredNumber)
+    }
+
+    function confirmInputHandler() {
+
+    }
+
     return <View style={styles.inputContainer}>
         <TextInput 
             style={styles.numberInput} 
             maxLength={2} 
             keyboardType="number-pad" 
             autoCapitalize="none"
-            autoCorrect={false} 
+            autoCorrect={false}
+            value={enteredNumber}
+            onChangeText={numberInputHandler}
         />
         <View style={styles.buttonsContainer}>
             <View style={styles.buttonContainer}>
                 <PrimaryButton>Reset</PrimaryButton>
             </View>
             <View style={styles.buttonContainer}>
-                <PrimaryButton>Confirm</PrimaryButton>
+                <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
             </View>
         </View>
     </View>
