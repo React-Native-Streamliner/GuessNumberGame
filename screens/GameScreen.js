df3c6558f152c14484ac4969f7bb79e3a81b5308
tiger <thisisstreamliner@gmail.com>
2023-08-11 18:56:52 +0300

    Creating NumberContainer

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,7 @@
 import { View, Text, StyleSheet } from "react-native"
-import Title from "../components/Title"
+import Title from "../components/ui/Title"
+import { useState } from "react"
+import NumberContainer from "../components/game/NumberContainer"
 
 function generateRandomBetween(min, max, exclude) {
     const rndNum = Math.floor(Math.random() * (max - min)) + min
@@ -11,9 +13,14 @@ function generateRandomBetween(min, max, exclude) {
     }
 }
 
-function GameScreen() {
+function GameScreen({userNumber}) {
+
+    const initialGuess = generateRandomBetween(1, 100, userNumber)
+    const[currentGuess, setCurrentGuess] = useState(initialGuess) 
+
     return <View style={styles.screen}>
         <Title>Opponent's Guess</Title>
+        <NumberContainer>{currentGuess}</NumberContainer>
         <View>
             <Text>Higher or lower?</Text>
         </View>
