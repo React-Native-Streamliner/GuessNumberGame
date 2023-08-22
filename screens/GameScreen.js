5e47b19fc7a9a85dd4603db3c69636f2a3655c4c
tiger <thisisstreamliner@gmail.com>
2023-08-22 23:46:25 +0300

    Calculate GuessRounds

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -30,7 +30,7 @@ function GameScreen({userNumber, onGameOver}) {
 
     useEffect(() => {
         if (currentGuess === userNumber) {
-            onGameOver()
+            onGameOver(guessRounds.length)
         }
     }, [currentGuess, userNumber, onGameOver])
 
