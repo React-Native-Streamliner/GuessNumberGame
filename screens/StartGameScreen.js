e08611e6527dd2139fc29a9d6ba59995e85a10f4
tiger <thisisstreamliner@gmail.com>
2023-08-26 11:01:33 +0300

    Setting Sizes Dynamically (For Different Orientations)

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { TextInput, View, StyleSheet, Alert, Text } from "react-native"
+import { TextInput, View, StyleSheet, Alert, useWindowDimensions } from "react-native"
 import PrimaryButton from "../components/ui/PrimaryButtons"
 import Colors from '../const/colors'
 import Title from '../components/ui/Title'
@@ -9,6 +9,8 @@ import Card from '../components/ui/Card'
 
 function StartGameScreen({onPickNumber}) {
     const [enteredNumber, setEnteredNumber] = useState('')
+
+    const { width, height } = useWindowDimensions()
 
     function resetInputHandler() {
         setEnteredNumber('')
@@ -38,7 +40,9 @@ function StartGameScreen({onPickNumber}) {
         onPickNumber(chosenNumber)
     }
 
-    return <View style={styles.rootContainer}>
+    const marginTopDistance = height < 380 ? 30 : 100
+
+    return <View style={[styles.rootContainer, {marginTop: marginTopDistance}]}>
         <Title>Guess My Number</Title>
         <Card>
             <InstructionText>Enter a Number:</InstructionText>
@@ -65,10 +69,11 @@ function StartGameScreen({onPickNumber}) {
 
 export default StartGameScreen
 
+const deviceHeight = Dimensions.get('window').height
+
 const styles = StyleSheet.create({
     rootContainer: {
         flex: 1,
-        marginTop: 100,
         alignItems: 'center'
     },
     numberInput: {
